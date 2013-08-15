07ae02d28d9f3b782d2b3a26d0334771036c3ea7
George Xie <george@xiegeo.com>
2013-08-15 02:51:28 -0700

    comments

--- src/data/bitset/file_backed_bitset.go
+++ src/data/bitset/file_backed_bitset.go
@@ -13,6 +13,10 @@ const (
 FileBackedBitSet assumes that OS intelligently cache file reads,
 and flashes on demand. BitSets are nomally used as metadata, so
 it should only flash after the main data flashes.
+
+Currently, it appears that file read can be an expansive operation,
+cached or not. A solution is pushed back until usage pattern is 
+more apparent.
 */
 type FileBackedBitSet struct {
 	f       *os.File
