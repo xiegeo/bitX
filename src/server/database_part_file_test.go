5a91d01078691205c025d8266b674ce0d36f073b
George Xie <george@xiegeo.com>
2013-08-20 06:46:23 -0700

    test when inner hashes are received in strange ways

--- src/server/database_part_file_test.go
+++ src/server/database_part_file_test.go
@@ -21,9 +21,43 @@ func TestPart(t *testing.T) {
 	test(1025)
 	test(2345)
 	test(12345)
+
+	source.Close()
+	part.Close()
 }
 
+type testPOOO struct {
+	req   network.InnerHashes
+	count hashtree.Nodes
+}
 
+func TestPartOutOfOrder(t *testing.T) {
+	source, part := testSetUp(t)
+
+	id := source.ImportFromReader(&testFile{length: 6 * hashtree.FILE_BLOCK_SIZE})
+	testPartStart(part, id, t)
+
+	testData := []testPOOO{
+		{network.NewInnerHashes(0, 0, 5, nil), 0},         //nothing can be verified
+		{network.NewInnerHashes(0, 1, 5, nil), 0},         //nothing can be verified
+		{network.NewInnerHashes(2, 0, 2, nil), 2},         //top two
+		{network.NewInnerHashes(0, 1, 5, nil), 2 + 3},     //plus the last two and parent, 1 to 3 lost
+		{network.NewInnerHashes(0, 0, 3, nil), 2 + 3},     //but nothing here
+		{network.NewInnerHashes(0, 0, 4, nil), 2 + 3 + 6}, //finnally
+	}
+	for _, v := range testData {
+		n, _, err := testTransfer(v.req, id, source, part, t)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if n != v.count {
+			t.Fatalf("count got:%v, should be %v, for test:%v", n, v.count, v.req.String())
+		}
+	}
+
+	source.Close()
+	part.Close()
+}
 
 func testSetUp(t *testing.T) (source Database, part Database) {
 	sourceDatabase := ".testSourceDatabase"
@@ -64,21 +98,26 @@ func testPartSize(size hashtree.Bytes, source Database, part Database, t *testin
 	if leafs == 1 {
 		req = network.NewInnerHashes(0, 0, 0, nil)
 	}
-
-	t.Logf("req:%v", req.String())
-	hashes, err := source.GetInnerHashes(id, req)
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Logf("hashes:%v", hashes.String())
-	_, complete, err2 := part.PutInnerHashes(id, hashes)
-	if err2 != nil {
-		t.Fatal(err2)
-	}
+	_, complete, _ := testTransfer(req, id, source, part, t)
 	if !complete {
 		t.Fatal("should have put all inner hashes in")
 	}
 
+}
+
+func testTransfer(req network.InnerHashes, id network.StaticId, source Database, part Database, t *testing.T) (has hashtree.Nodes, complete bool, err error) {
+	t.Logf("req:%v", req.String())
+	hashes, err := source.GetInnerHashes(id, req)
+	if err != nil {
+		t.Error(err)
+		return -1, false, err
+	}
+	t.Logf("hashes:%v", hashes.String())
+	n, complete, err2 := part.PutInnerHashes(id, hashes)
+	if err2 != nil {
+		t.Error(err2)
+	}
+	return n, complete, err2
 }
 
 func testPartStart(part Database, id network.StaticId, t *testing.T) {
