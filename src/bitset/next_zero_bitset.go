9447d2a7de393998fbc0027b27f385096dd58f47
George Xie <george@xiegeo.com>
2013-10-08 23:29:56 -0700

    added a ranged version and tested

--- src/bitset/next_zero_bitset.go
+++ src/bitset/next_zero_bitset.go
@@ -2,17 +2,44 @@ package bitset
 
 // a bitset for list the index of 0s
 type NextZeroBitSet struct {
-	SimpleBitSet
+	*SimpleBitSet
 	pos int
+}
+
+func NewNextZeroBitSet(s *SimpleBitSet) *NextZeroBitSet {
+	return &NextZeroBitSet{s, 0}
 }
 
 // the next 0 bit, indexed from 0 to cap-1, index is undefined if done
 func (n *NextZeroBitSet) Next() (index int, done bool) {
 	for ; n.pos < n.Capacity(); n.pos++ {
 		if n.Get(n.pos) {
-			return n.pos, false
+			n.pos++
+			return n.pos - 1, false
 		}
 		// todo: skip words with all 1s
 	}
 	return -1, true
+}
+
+// the next 0 bits, length is the number of consecutive 0s including the first,
+// done (see Next()) if length = 0
+func (n *NextZeroBitSet) NextRange(maxRange int) (index int, length int) {
+	index, done := n.Next()
+	if done {
+		return index, 0
+	} else {
+		r := 1
+		i, d := n.Next()
+		for r < maxRange && i == index+r && !d {
+			r++
+			i, d = n.Next()
+		}
+		if !d {
+			n.pos--
+		}
+
+		return index, r
+	}
+
 }
---
+++ src/bitset/next_zero_bitset_test.go
@@ -0,0 +1,29 @@
+package bitset
+
+import (
+	"testing"
+)
+
+func TestRange(t *testing.T) {
+	n := NewNextZeroBitSet(NewSimple(100))
+	starts := []int{0, 2, 20, 98}
+	lengths := []int{1, 5, 30, 2}
+	maxRange := 10
+	rstarts := []int{0, 2, 20, 30, 40, 98, -1}
+	rlengths := []int{1, 5, 10, 10, 10, 2, 0}
+	for k, s := range starts {
+		l := lengths[k]
+		for i := 0; i < l; i++ {
+			n.Set(s + i)
+		}
+	}
+
+	for k, s := range rstarts {
+		l := rlengths[k]
+		index, length := n.NextRange(maxRange)
+		if s != index || l != length {
+			t.Errorf("expect:%v, %v got:%v, %v ", s, l, index, length)
+		}
+	}
+
+}
